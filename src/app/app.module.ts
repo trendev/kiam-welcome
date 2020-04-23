3004b3d2b546867aaeb295ad0b3ff23f0e8b40d7
Julien Sié <julien.sie@gmail.com>
2020-04-23 16:16:59 +0200

    remove use faq components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,7 @@ import { FAQComponent } from './faq/faq.component';
 import { ApplicationQuestionsComponent } from './faq/application-questions/application-questions.component';
 import { FirstTimeQuestionsComponent } from './faq/first-steps-questions/first-steps-questions.component';
 import { SecurityQuestionsComponent } from './faq/security-questions/security-questions.component';
-import { OfferingQuestionsComponent } from './faq/offering-questions/offering-questions.component';
 import { ClientsQuestionsComponent } from './faq/clients-questions/clients-questions.component';
-import { CollectiveGroupsQuestionsComponent } from './faq/collective-groups-questions/collective-groups-questions.component';
 import { BillsQuestionsComponent } from './faq/bills-questions/bills-questions.component';
 import { SubscriptionQuestionsComponent } from './faq/subscription-questions/subscription-questions.component';
 
@@ -26,9 +24,7 @@ import { SubscriptionQuestionsComponent } from './faq/subscription-questions/sub
     ApplicationQuestionsComponent,
     FirstTimeQuestionsComponent,
     SecurityQuestionsComponent,
-    OfferingQuestionsComponent,
     ClientsQuestionsComponent,
-    CollectiveGroupsQuestionsComponent,
     BillsQuestionsComponent,
     SubscriptionQuestionsComponent
   ],
