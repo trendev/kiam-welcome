d66c7de1e57ed111c41e5343c5befdac74cb4c7d
Julien Sié <julien.sie@gmail.com>
2020-04-23 17:58:25 +0200

    bills faq

--- src/app/faq/bills-questions/bills-questions.component.ts
+++ src/app/faq/bills-questions/bills-questions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bills-questions',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bills-questions.component.scss']
 })
 export class BillsQuestionsComponent implements OnInit {
+
+  @Input() appName = 'kiam';
+  @Input() expanded = true;
 
   constructor() { }
 
