a4e34a83f0eebb44faa780963f5b96ea1e1bd5e7
Julien Sié <julien.sie@gmail.com>
2020-06-26 18:48:34 +0200

    format feedbacks

--- src/app/home/feedback/feedback.component.ts
+++ src/app/home/feedback/feedback.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
-  @Input() feedback: Feedback;
+  @Input() fdk: Feedback;
 
   constructor() { }
 
--- src/app/home/feedback/feedbacks.data.ts
+++ src/app/home/feedback/feedbacks.data.ts
@@ -1,13 +1,36 @@
 import { Feedback } from './feedback.model';
 export const feedbacks: Feedback[] = [
     {
-        author: 'Marion',
-        jobrole: 'Coiffeuse',
+        author: 'Vanessa',
+        jobrole: 'Coiffeuse/Esthéticienne (77)',
         content: [
-            'J’utilise l’application depuis plus de deux ans et je la trouve vraiment géniale!',
-            'Elle est tout à fait adaptée aux micro-entreprises!',
-            'Je crée des factures en quelques clics et je les envoie directement à mes clients par SMS ou sur WhatsApp. Je trouve ça plus moderne que d’utiliser un facturier papier ou Excel...',
-            'L’application calcule automatiquement mon chiffre d’affaire et je me contente aujourd’hui d’un simple copier/coller pour ma déclaration à l’URSSAF. Je fais entièrement confiance à l’application, c’est beaucoup plus pratique que d’éplucher toutes mes factures pendant 3 jours et j’évite de faire des erreurs...'
+            'J’utilise l’application depuis plus de deux ans et je la trouve vraiment bien !',
+            'Elle est tout à fait adaptée aux micro-entreprises !',
+            'Je crée des factures en seulement quelques clics et je les envoie directement à mes clients par SMS ou sur WhatsApp.',
+            'Je trouve ça plus moderne que d’utiliser un facturier papier ou Excel...',
+            'Elle calcule automatiquement mon chiffre d’affaire et je me contente aujourd’hui d’un simple copier/coller pour ma déclaration à l’URSSAF. Je fais entièrement confiance à l’application, c’est beaucoup plus pratique que d’éplucher toutes mes factures pendant 3 jours et j’évite de faire des erreurs...'
+        ]
+    },
+    {
+        author: 'Yassine',
+        jobrole: 'VTC indépendant (92)',
+        content: [
+            'J’ai découvert l’application en transportant son concepteur en 2019.',
+            'Je propose régulièrement mes services à des comités d’entreprise et je trouve très pratique de pouvoir les facturer directement.',
+            'Grâce à l’application, je connais rapidement mon chiffre d’affaire pour l’ensemble de l’entreprise et ça depuis la fiche de l’entreprise !',
+            'Je n’ai plus besoin de faire ma compta client par client comme avant : c’est une vraie économie de temps !',
+            'Plus besoin de 50 applications comme mes copains « Loti »...'
+        ]
+    },
+    {
+        author: 'José',
+        jobrole: 'Électricien (94)',
+        content: [
+            'Licencié en 2011, j’ai décidé de monter ma société pour être indépendant et autonome.',
+            'J’ai déjà utilisé une bonne dizaine d’applications de facturation gratuites ou payantes... Je pense qu’elles ne sont pas adaptées, parfois trop complexes ou trop limitées.',
+            'En cherchant sur internet, j’ai récemment trouvé cette application. J’ai demandé une démonstration de l’application en cliquant sur le bouton du support.',
+            'J’aime bien le fait de pouvoir utiliser l’application depuis mon téléphone (je suis plus souvent sur mes chantiers qu’au bureau) ou sur mon ordinateur.',
+            'Franchement, c’est facile, c’est rapide, c’est « sécurisé dans le Cloud » : je recommande chaudement !'
         ]
     }
 ];
